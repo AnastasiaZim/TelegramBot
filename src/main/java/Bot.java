4536cd5dee7371f030e38b9660c87a2d325f513c
Анастасия <zimnitskayasakyra@mail.ru>
2019-10-20 05:02:47 +0300

    it is work! But telegram has blocked, i had to install VPN.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,11 +1,15 @@
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 public class Bot extends TelegramLongPollingBot {
     public static void main(String[] args) {
+        org.apache.log4j.BasicConfigurator.configure();
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
@@ -15,8 +19,33 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-    public void onUpdateReceived(Update update) {
+    private void sendMsg(Message message, String text){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            switch (message.getText()) {
+                case "/help":
+                    sendMsg(message,"Помогаю");
+                    break;
+                case "/lapka":
+                    sendMsg(message,"У меня тоже");
+                    break;
+                default:
+                    sendMsg(message,"Ничего не поняла, но очень рада за вас");
+            }
+        }
     }
 
     public String getBotUsername() {
