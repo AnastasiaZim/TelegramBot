11a80efe28f332db712ad6b43b258e6611e5d956
Анастасия <zimnitskayasakyra@mail.ru>
2019-10-21 01:59:16 +0300

    reply and inline keyboard

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -6,19 +6,28 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
     private Bot() {
         super(getBotOptions());
     }
 
-    private static DefaultBotOptions getBotOptions(){
-        DefaultBotOptions botOptions= ApiContext.getInstance(DefaultBotOptions.class);
-        botOptions.setProxyHost("129.146.181.251");
-        botOptions.setProxyPort(3128);
-        botOptions.setProxyType(DefaultBotOptions.ProxyType.HTTP);
+    private static DefaultBotOptions getBotOptions() {
+        DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
+        botOptions.setProxyHost("47.91.88.100");
+        botOptions.setProxyPort(1080);
+        botOptions.setProxyType(DefaultBotOptions.ProxyType.SOCKS5);
         return botOptions;
     }
 
@@ -32,35 +41,96 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-    private void sendMsg(Message message, String text){
+    private void sendMsg(Message message, String text) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
         sendMessage.setChatId(message.getChatId());
-//        sendMessage.setReplyToMessageId(message.getMessageId());
         sendMessage.setText(text);
         try {
+            setButtons(sendMessage);
             execute(sendMessage);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
     }
 
+    @Override
     public void onUpdateReceived(Update update) {
-        Message message = update.getMessage();
-        if (message != null && message.hasText()) {
-            switch (message.getText()) {
-                case "/help":
-                    sendMsg(message,"Помогаю");
-                    sendMsg(message,"(нет)");
-                    break;
-                case "/lapka":
-                    sendMsg(message,"У меня тоже");
-                    break;
-                default:
-                    sendMsg(message,"Ничего не поняла, но очень рада за вас");
+        if (update.hasMessage()) {
+            if (update.getMessage().hasText()) {
+                switch (update.getMessage().getText()) {
+                    case "/start":
+                        sendMsg(update.getMessage(), "Привет, я бот, можешь понажимать на мои кнопочки");
+                        break;
+                    case "/hello":
+                        sendMsg(update.getMessage(), "hello");
+                        break;
+                    case "/time":
+                        int hour = Calendar.getInstance().get(Calendar.HOUR);
+                        int min = Calendar.getInstance().get(Calendar.MINUTE);
+                        sendMsg(update.getMessage(), hour + ":" + min);
+                        break;
+                    case "/info":
+                        try {
+                            execute(sendInlineKeyBoardMessage(update.getMessage().getChatId()));
+                        } catch (TelegramApiException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    default:
+                        sendMsg(update.getMessage(), "Не поняла ничего");
+                }
+            }
+        } else if (update.hasCallbackQuery()) {
+            try {
+                execute(new SendMessage().setText(
+                        update.getCallbackQuery().getData())
+                        .setChatId(update.getCallbackQuery().getMessage().getChatId()));
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
             }
         }
     }
+
+
+    private void setButtons(SendMessage sendMessage) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        // InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        // sendMessage.setInlineMarkup(inlineKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        keyboardFirstRow.add(new KeyboardButton("/hello"));
+        keyboardFirstRow.add(new KeyboardButton("/time"));
+        keyboardFirstRow.add(new KeyboardButton("/info"));
+        keyboardRowList.add(keyboardFirstRow);
+        replyKeyboardMarkup.setKeyboard(keyboardRowList);
+
+    }
+
+    private static SendMessage sendInlineKeyBoardMessage(long chatId) {
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
+        InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton();
+        inlineKeyboardButton1.setText("Автор");
+        inlineKeyboardButton1.setCallbackData("Анастасия Зимницкая");
+        inlineKeyboardButton2.setText("Группа");
+        inlineKeyboardButton2.setCallbackData("2-пми-4");
+        List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardButtonsRow2 = new ArrayList<>();
+        keyboardButtonsRow1.add(inlineKeyboardButton1);
+        keyboardButtonsRow1.add(new InlineKeyboardButton().setText("Какой год?").setCallbackData("На дворе "+Calendar.getInstance().get(Calendar.YEAR)));
+        keyboardButtonsRow2.add(inlineKeyboardButton2);
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        rowList.add(keyboardButtonsRow1);
+        rowList.add(keyboardButtonsRow2);
+        inlineKeyboardMarkup.setKeyboard(rowList);
+        return new SendMessage().setChatId(chatId).setText("Кнопочки").setReplyMarkup(inlineKeyboardMarkup);
+    }
+
 
     public String getBotUsername() {
         return "MyTestBot";
