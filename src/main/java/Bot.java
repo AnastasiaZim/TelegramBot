2e75330813f3306b6578478de93c710af267a3b6
Анастасия <zimnitskayasakyra@mail.ru>
2019-10-20 03:35:07 +0300

    Initial commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,29 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+public class Bot extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    public String getBotUsername() {
+        return "MyTestBot";
+    }
+
+    public String getBotToken() {
+        return "842016954:AAHwHJRQo6zjixPnbZBBp0J9UHU4Ly0M6eY";
+    }
+}
